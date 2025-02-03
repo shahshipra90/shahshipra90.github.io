f3ab8930bf9423cfface5ad53754bdd875fd3f3b
shahshipra90 <65304359+shahshipra90@users.noreply.github.com>
2025-02-03 12:52:46 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,38 +26,49 @@ const footer = () => {
           <div className="phone">
             <h4>
               {" "}
+                <a href="tel:+15199911752" style={{ color: "#fff", textDecoration: "none" }}>
               <FaPhone
                 size={20}
                 style={{ color: "#fff", marginRight: "2rem" }}
               />
               +1 (519)-991-1752
+                </a>
             </h4>
           </div>
           <div className="email">
             <h4>
-              {" "}
+              {" "}<a href="mailto:shahshipra278@gmail.com" style={{ color: "#fff", textDecoration: "none" }}>
               <FaMailBulk
                 size={20}
                 style={{ color: "#fff", marginRight: "2rem" }}
               />
               shahshipra278@gmail.com
+                </a>
             </h4>
           </div>
         </div>
         <div className="right">
           <div className="social">
-            <FaLinkedin
+             <a
+              href="www.linkedin.com/in/shiprashah90"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+                <FaLinkedin
               size={30}
               style={{ color: "#fff", marginRight: "1rem" }}
             />
+              </a>
+          <a
+          href="https://github.com/shahshipra90"
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
             <FaGithub
               size={30}
               style={{ color: "#fff", marginRight: "1rem" }}
             />
-            <FaInstagram
-              size={30}
-              style={{ color: "#fff", marginRight: "1rem" }}
-            />
+              </a>
           </div>
         </div>
       </div>
