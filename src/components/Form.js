7d3c0d128eddc3b0ef53584f39791e2aa89ac0f5
shahshipra90 <65304359+shahshipra90@users.noreply.github.com>
2025-02-03 12:59:36 -0500

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import { FaHandshake, FaMailBulk, FaLinkedin, FaInstagram, FaGithub, FaPhone } from "react-icons/fa"
+import { FaHandshake, FaMailBulk, FaLinkedin, FaInstagram, FaGithub, FaPhone, FaEnvelope } from "react-icons/fa"
 import "./FormStyles.css"
 import React from 'react'
 
@@ -14,10 +14,10 @@ const Form = () => {
         <p>Here are a few ways you can contact me, whether you have a once-in-a-lifetime<br></br> opportunity or just some ideas! If I don't respond right away, I might be out exploring<br></br> in the middle of nowhere. I'll get back to you as soon as I can. That's for sure!</p>
       </div>
       <div className="connect-ways">
-        <FaPhone size={30} style={{color: "#fff", marginRight: "1rem"}} />
-        <FaLinkedin size={30} style={{color: "#fff", marginRight: "1rem"}} />
-        <FaGithub size={30} style={{color: "#fff", marginRight: "1rem"}} />
-        <FaInstagram size={30} style={{color: "#fff", marginRight: "1rem"}} />
+        <a href="tel:+15199911752" style={{ color: "#fff", textDecoration: "none" }}><FaPhone size={30} style={{color: "#fff", marginRight: "1rem"}} /></a>
+        <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer"><FaLinkedin size={30} style={{color: "#fff", marginRight: "1rem"}} /></a>
+        <a href="https://github.com/your-profile" target="_blank" rel="noopener noreferrer"><FaGithub size={30} style={{color: "#fff", marginRight: "1rem"}} /></a>
+        <a href="mailto:shahshipra278@gmail.com" style={{ color: "#fff", textDecoration: "none" }}><FaEnvelope size={30} style={{color: "#fff", marginRight: "1rem"}} /></a>
       </div>
       </div>
     </div>
